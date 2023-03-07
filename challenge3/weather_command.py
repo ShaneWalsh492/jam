22795bf9961a188bbf08db88eb0d6009e201698a
Shane Walsh <G00406694@gmit.ie>
2023-03-07 16:10:28 +0000

    Update weather_command.py

--- challenge3/weather_command.py
+++ challenge3/weather_command.py
@@ -18,33 +18,63 @@ measurements = {
 def weather(message):
     message = message.lower()
 
-    # Let's build the API call!
+    if len(message) > 0:
+        if len(message.split(' ')) > 1:
+            city = message.split(' ')[0]
+            measure = message.split(' ')[1]
+        else:
+            city = message
+            measure = ""
 
-    # For challenge 3.1, you'll need to get the longitude and latitude for Galway
-    # and put them in the params below.
+        if city in city_coords.keys():
+            if len(measure) > 0:
+                if measure in measurements.keys():
+                    date_yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+                    params = {
+                        'latitude': city_coords[city][0],  # You need to update this
+                        'longitude': city_coords[city][1], # ... and this
+                        'start_date': date_yesterday,
+                        'end_date': date_yesterday,
+                        'timezone': 'GMT',
+                        'daily': measurements[measure]
+                    }
 
-    # For 3.2, looks like you'll need a mapping of cities to coordinates. Check out
-    # city_coords above, and wire it up to solve this challenge!
+                    response = requests.get('https://api.open-meteo.com/v1/forecast', params=params).json()
+                    return response["daily"][measurements[measure]][0]
+                else:
+                    return "Invalid Measurement"
+            else:
+                date_yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+                params = {
+                    'latitude': city_coords[message][0],  # You need to update this
+                    'longitude': city_coords[message][1], # ... and this
+                    'start_date': date_yesterday,
+                    'end_date': date_yesterday,
+                    'timezone': 'GMT',
+                    'daily': 'temperature_2m_max'
+                }
 
-    # 3.3 doesn't really introcude any new concepts. You're on your own!
+                response = requests.get('https://api.open-meteo.com/v1/forecast', params=params).json()
+                return response["daily"]["temperature_2m_max"][0]
+            
+        else:
+            return "Invalid City!"
+    else:
+        # For challenge 3.1, you'll need to get the longitude and latitude for Galway
+        # and put them in the params below.
 
-    date_now = (datetime.now() - timedelta(days=-1)).strftime('%Y-%m-%d')
-    daily_params = ['temperature_2m_max', 'rain_sum', 'windspeed_10m_max']
-    params = {
-        'latitude': 0,  # You need to update this
-        'longitude': 0, # ... and this
-        'start_date': date_now,
-        'end_date': date_now,
-        'timezone': 'GMT',
-        'daily': daily_params
-    }
+        date_yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+        daily_params = ['temperature_2m_max']
+        params = {
+            'latitude': 53.270668,  # You need to update this
+            'longitude': -9.056790, # ... and this
+            'start_date': date_yesterday,
+            'end_date': date_yesterday,
+            'timezone': 'GMT',
+            'daily': daily_params
+        }
 
-    # This makes a request to the weather API with the above info.
-    response = requests.get('https://api.open-meteo.com/v1/forecast', params=params).json()
+        response = requests.get('https://api.open-meteo.com/v1/forecast', params=params).json()
+        return response["daily"]["temperature_2m_max"][0]
 
-    # Let's print the response. Look out for this in your terminal, you'll need to pull
-    # out the bits of information that are relevant to the command used.
-    print(json.dumps(response, indent=4))
 
-    # This is a placeholder response to show how to drill into the info that you're interested in.
-    return response['daily']['time'][0]
